8d2515a53177de88b19dbe24f039b1bb03987a25
piyush <piyushujgaokar544@gmail.com>
2026-03-04 18:54:12 +0530

    Authentication frontend and backend complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { RouterProvider } from 'react-router'
+import { router } from './app.routes.jsx'
+import { AuthProvider } from './features/auth/auth.context.jsx'
 
 const App = () => {
   return (
-    <div></div>
+   <AuthProvider>
+     <RouterProvider router={router}/>
+   </AuthProvider>
   )
 }
 
---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,62 @@
+import { useContext, useEffect } from "react"
+import { AuthContext } from "../auth.context"
+import {register,login,logout,getme} from '../services/auth.api'
+
+
+export const useAuth = () => {
+
+    const context=useContext(AuthContext)
+    console.log(context.loading);
+    
+    const {user,setUser,loading,setLoading}=context
+
+    const handleLogin=async ({email,password})=>{
+        setLoading(true)
+        try {
+            const data= await login({email,password})
+            setUser(data.user)
+        } catch (error) {
+            console.log(error);
+        }finally{
+            setLoading(false)
+        }
+
+    }
+
+    const handleRegister=async ({username,email,password})=>{
+        setLoading(true)
+        try{
+            const data=await register({username,email,password})
+            setUser(data.user)
+        }catch(error){
+            console.log(error);    
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    const handleLogout=async ()=>{
+        setLoading(false)
+        try {
+            const data=await logout()
+            setUser(null)
+        } catch (error) {
+            console.log(error); 
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    useEffect(()=>{
+        const getAndSetUser=async ()=>{
+                const data=await getme()
+                setUser(data.user)
+                setLoading(false)
+        }
+
+        getAndSetUser()
+    },[])
+
+  return {user,loading,handleLogin,handleRegister,handleLogout}
+}
+
